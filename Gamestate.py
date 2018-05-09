213a7a86b8afbffc33252791e9a53bf6f85ebf94
maikel <mykldog@gmail.com>
2018-05-09 13:28:03 +0700

    Added class for cards

---
+++ Gamestate.py
@@ -0,0 +1,17 @@
+
+class card:
+    def __init__(self, col, val):
+        self.color = col
+        self.value = val
+    
+class playersideofboard:
+    def __init__(self):
+       pass
+       
+class discardarea:
+    def __init__(self):
+       pass
+    
+class gamestate:
+    def __init__(self):
+        print("gamestate") 
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import player
+import Gamestate
 
 a = player.player()
+g = Gamestate.gamestate()
+
+ash = Gamestate.card('B',5)
+
 print('done') 
